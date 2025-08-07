14263aa294cf0cdfba0872da60df9e198ceec6e3
StrawberrySweet22 <128013731+StrawberrySweet22@users.noreply.github.com>
2025-08-07 14:07:12 -0500

    moved non dependent files to e_proj. Created grid processing for e_proj

---
+++ e_proj/grid_count.py
@@ -0,0 +1,61 @@
+import numpy as numpy
+
+def cardinal_counter(pos, direction_vec, n, grid, grid_m):
+	card_list = []
+	for i in range(n):
+		if((not(pos[0]+i*direction_vec[0] < 0) and not(pos[1]+i*direction_vec[1] < 0)) and (not(pos[0]+i*direction_vec[0] > grid_m-n) and not(pos[1]+i*direction_vec[1] > grid_m-n))):
+			card_list.append(grid[pos[0]+i*direction_vec[0]][pos[1]+i*direction_vec[1]])
+		else:
+			return card_list
+	return card_list
+
+def read_sdf(file_path):
+	read_arr = []
+	read_stream = open(file_path)
+
+	line = []
+	val = []
+
+	for i in read_stream.read():
+		if(i == ' '):
+			val = val[0]*10+val[1]
+			line.append(val)
+			val = []
+		elif(i == '\n'):
+			read_arr.append(line)
+			line = []
+		else:
+			val.append(int(i))
+	read_stream.close()
+	return read_arr
+
+
+
+arr = read_sdf("_grid_count_data.sdf")
+dir_arr = []
+
+#for i in arr:
+#	for j in i:
+for i in range (3):
+	for j in range(3):
+		dir_vec = [i%3-1,j-1]
+		if(dir_vec != [0,0]):
+			dir_arr.append(dir_vec)
+pos = [0,0]
+
+max_val = 0
+
+for i in arr:
+	for j in i:
+		for k in dir_arr:
+			cur_arr = cardinal_counter(pos, k, 4, arr, len(arr))
+			cur_prod = 1
+			for h in cur_arr:
+				cur_prod = cur_prod*h
+			if(cur_prod > max_val):
+				max_val = cur_prod
+		pos[1] += 1
+	pos[0] += 1
+	pos[1] = 0
+
+print(max_val)
--- e_proj/prime.py
+++ e_proj/prime.py
@@ -2,11 +2,17 @@ import numpy as np
 import timeit
 import matplotlib.pyplot as plt
 
-def sieve(n):
+def sieve(n=False, n_max = False):
 	num_list = []
-	if(n > 6):
+	tester = int(6*(np.log(6)+np.log(np.log(6))))
+	if(not(n_max)):
 		n_max = int(n*(np.log(n)+np.log(np.log(n))))
 		sqr_n = int(np.sqrt(n))
+	else:
+		sqr_n = int(np.sqrt(n_max))
+	if(n < 6 and n_max < tester):
+		return[2, 3, 5, 7, 11, 13]
+	else:
 		num_list = sieve(sqr_n)
 		test_list = list(num_list)
 		for i in range(max(num_list),n_max):
@@ -25,23 +31,9 @@ def sieve(n):
 					if j%i == 0:
 						num_list.remove(j)
 		return num_list	
-	else:
-		return[2, 3, 5, 7, 11, 13]
 
 start = timeit.default_timer()
-x_plot = []
-y_plot = []
-for i in range(100000):
-	array_start = timeit.default_timer()
-	sieve(i)
-	array_stop = timeit.default_timer()
-	x_plot.append(i)
-	array_tot = array_stop - array_start
-	y_plot.append(array_tot)
-	
-plot(x_plot, y_plot)
-plt.show()
-
+print(sum(sieve(False, 2000000)))
 stop = timeit.default_timer()
 
 print('Time: ', stop - start)  
